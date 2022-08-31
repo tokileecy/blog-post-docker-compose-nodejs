9112ffa43a729043eaa3b18841069439feffcd58
tokileecy <tokileecy@gmail.com>
2022-08-31 15:27:12 +0800

    feat: add my-project-3

---
+++ my-project-3/apps/backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8081, () => {
+  console.log(`Server running on port 8081`);
+});
